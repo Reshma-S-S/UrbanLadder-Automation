5163c416c4f0ff4b2e43000dc9998f284dbbe6ca
S S <2370057@cognizant.com>
2026-06-08 11:47:13 +0530

    New changes

--- src/test/java/com/cts/urbanladder/test/TestBase.java
+++ src/test/java/com/cts/urbanladder/test/TestBase.java
@@ -4,72 +4,121 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.time.Duration;
 import java.util.Date;
 import java.util.Properties;
 
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeOptions;
+//import org.openqa.selenium.firefox.FirefoxOptions;
+
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class TestBase {
-	public static WebDriver driver;
-	public String location=System.getProperty("user.dir")+"//src//test//resources//Data.xlsx";
-	
-	@BeforeClass
-	@Parameters({"browser"})
-	void setup(@Optional("chrome") String br) throws IOException {
-		Properties properties = new Properties();
-	    FileInputStream inputStream = new FileInputStream("src/test/resources/config.properties");
-	    properties.load(inputStream);
-	    String url=properties.getProperty("url");
-	    switch(br.toLowerCase()) {
-	    	case "chrome":
-	    		driver=new ChromeDriver();
-	    		break;
-	    	case "edge":
-	    		driver=new EdgeDriver();
-	    		break;
-	    		
-	    	default:
-                System.out.println("Invalid browser name."); // Print message if invalid browser name
-                return;
-	    }
-		driver.get(url);
-		driver.manage().window().maximize();
-	}
-	public String captureScreen(String tname) throws IOException { // Method to capture screenshot
-		
-		if (driver == null) {
+
+    public static WebDriver driver;
+    public String location = System.getProperty("user.dir") + "/src/test/resources/Data.xlsx";
+
+    @BeforeClass
+    @Parameters({ "browser" })
+    public void setup(@Optional("chrome") String br) throws IOException {
+
+        // ✅ Load properties file properly
+        Properties properties = new Properties();
+        FileInputStream inputStream = new FileInputStream(
+                System.getProperty("user.dir") + "/src/test/resources/config.properties");
+
+        properties.load(inputStream);
+
+        String url = properties.getProperty("url");
+        boolean isHeadless = Boolean.parseBoolean(properties.getProperty("headless"));
+
+        System.out.println("Launching Browser: " + br);
+        System.out.println("Headless Mode: " + isHeadless);
+
+        // ✅ Cross-browser setup with WebDriverManager
+        switch (br.toLowerCase()) {
+
+            case "chrome":
+                WebDriverManager.chromedriver().setup();
+
+                ChromeOptions chromeOptions = new ChromeOptions();
+                if (isHeadless) {
+                    chromeOptions.addArguments("--headless=new");
+                    chromeOptions.addArguments("--window-size=1920,1080");
+                }
+
+                driver = new ChromeDriver(chromeOptions);
+                break;
+
+            case "edge":
+                WebDriverManager.edgedriver().setup();
+
+                EdgeOptions edgeOptions = new EdgeOptions();
+                if (isHeadless) {
+                    edgeOptions.addArguments("--headless=new");
+                    edgeOptions.addArguments("--window-size=1920,1080");
+                }
+
+                driver = new EdgeDriver(edgeOptions);
+                break;
+
+            case "firefox":
+                WebDriverManager.firefoxdriver().setup();
+                driver = new FirefoxDriver();
+                break;
+
+            default:
+                System.out.println("Invalid browser name: " + br);
+                throw new RuntimeException("Invalid browser");
+        }
+
+        // ✅ Important setup
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().window().maximize();
+
+        driver.get(url);
+    }
+
+    // ✅ FIXED Screenshot Method
+    public String captureScreen(String tname) throws IOException {
+
+        if (driver == null) {
             throw new IllegalStateException("Driver is not initialized.");
         }
-		
-        String timeStamp = new SimpleDateFormat("yyyyMMddhhmmss").format(new Date()); // Generate timestamp for screenshot file name
-        TakesScreenshot takesScreenshot = (TakesScreenshot) driver; // Cast WebDriver to TakesScreenshot
-        File sourceFile = takesScreenshot.getScreenshotAs(OutputType.FILE); // Capture screenshot as file
 
-        String targetFilePath = System.getProperty("user.dir") + "\\screenshots\\" + tname + "_" + timeStamp + ".png"; // Define target file path
-        File targetFile = new File(targetFilePath); // Create target file
+        String timeStamp = new SimpleDateFormat("yyyyMMddHHmmss").format(new Date());
 
-        sourceFile.renameTo(targetFile); // Rename source file to target file
+        TakesScreenshot ts = (TakesScreenshot) driver;
+        File sourceFile = ts.getScreenshotAs(OutputType.FILE);
 
-        return targetFilePath; // Return target file path
+        String targetPath = System.getProperty("user.dir") + "/screenshots/" + tname + "_" + timeStamp + ".png";
+
+        File targetFile = new File(targetPath);
+
+        FileUtils.copyFile(sourceFile, targetFile); // ✅ FIXED
+
+        return targetPath;
     }
-	
-	@AfterClass
-	void tearDown() {
-		if (driver != null) {
+
+    @AfterClass
+    public void tearDown() {
+        if (driver != null) {
             driver.quit();
         }
-	}
-	
-	
-	
-
-
+    }
 }
